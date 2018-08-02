6fb5029517393cb09212b5993f8eff4fc37f4e9f
fakhir-shad <fakhir.shad@virtual-force.com>
2018-08-02 11:59:55 +0500

    #fixed method issues

--- lib/zoomify/resources/cloud_recording.rb
+++ lib/zoomify/resources/cloud_recording.rb
@@ -2,9 +2,8 @@ module Zoomify
   module Resources
     module CloudRecording
       def user_cloud_recordings *args
-        params = Request.extract_params args
-        Request.raise_user_id_error(params)
-        (raise Request.argument_error "from and to") if (params[:from].blank? || params[:to].blank?)
+        params = Request.extract_params_and_manage_user_id_error *args
+        Request.raise_from_to_error params
         self.class.fire_get("/users/#{Request.extract_id_from_params(params)}/recordings", params)
       end
       %w(meeting_cloud_recordings meeting_cloud_recordings_delete_all meeting_cloud_recordings_recover).each do |method|
@@ -24,40 +23,40 @@ module Zoomify
         end
       end
       private
-      def recording_with_meeting_id_options params
-        {
-            meeting_cloud_recordings:
-                {
-                    request: 'get',
-                    url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings"
-                },
-            meeting_cloud_recordings_delete_all:
-                {
-                    request: 'delete',
-                    url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings"
-                },
-            meeting_cloud_recordings_recover:
-                {
-                    request: 'put',
-                    url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/status"
-                }
-        }
-      end
+        def recording_with_meeting_id_options params
+          {
+              meeting_cloud_recordings:
+                  {
+                      request: 'get',
+                      url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings"
+                  },
+              meeting_cloud_recordings_delete_all:
+                  {
+                      request: 'delete',
+                      url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings"
+                  },
+              meeting_cloud_recordings_recover:
+                  {
+                      request: 'put',
+                      url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/status"
+                  }
+          }
+        end
 
-      def recording_with_multiple_ids params
-        {
-            meeting_cloud_recording_delete:
-                {
-                    request: 'delete',
-                    url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/#{params[:recording_id]}"
-                },
-            meeting_cloud_recording_recover:
-                {
-                    request: 'put',
-                    url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/#{params[:recording_id]}/status"
-                }
-        }
-      end
+        def recording_with_multiple_ids params
+          {
+              meeting_cloud_recording_delete:
+                  {
+                      request: 'delete',
+                      url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/#{params[:recording_id]}"
+                  },
+              meeting_cloud_recording_recover:
+                  {
+                      request: 'put',
+                      url: "/meetings/#{Request.extract_id_vs_uuid_from_params(params)}/recordings/#{params[:recording_id]}/status"
+                  }
+          }
+        end
     end
   end
 end
--- lib/zoomify/resources/user.rb
+++ lib/zoomify/resources/user.rb
@@ -4,79 +4,78 @@ module Zoomify
       %w(users user_create).each do |method|
         define_method method do |*args|
           params = Request.extract_params args
-          self.class.send(method_without_id_options[method.to_sym], '/users', params)
+          self.class.send(user_method_without_id_options[method.to_sym], '/users', params)
         end
       end
       %w(user user_update user_delete user_assistants user_assistants_create user_assistants_delete_all user_schedulers user_schedulers_delete_all).each do |method|
         define_method method do |*args|
-          params = Request.extract_params args
-          (raise Request.argument_error "id") if params[:id].blank?
-          method_option = method_with_id_options(params)[method.to_sym]
+          params = Request.extract_params_and_manage_user_id_error *args
+          method_option = user_method_with_id_options(params)[method.to_sym]
           self.class.send("fire_#{method_option[:request]}", method_option[:url], params)
         end
       end
       %w(user_assistant_delete user_schedulers_delete).each do |method|
         define_method method do |*args|
           params = Request.extract_params(args)
-          method_option = method_with_multiple_id_options(params)[method.to_sym]
-          (raise Request.argument_error "User and Assistant Id") if method_option[:condition]
+          method_option = user_method_with_multiple_id_options(params)[method.to_sym]
+          (raise Request.argument_error "User and #{method.split('_')[1].singularize.capitalize} Id") if method_option[:condition]
           self.class.fire_delete(method_option[:url], params)
         end
       end
 
       private
-      def method_without_id_options
-        {users: 'fire_get', user_create: 'fire_post'}
-      end
-      def method_with_id_options params
-        {
-            user: {
-                request: 'get',
-                url: "/users/#{Request.extract_id_from_params(params)}"
-            },
-            user_update: {
-                request: 'patch',
-                url: "/users/#{Request.extract_id_from_params(params)}"
-            },
-            user_delete: {
-                request: 'delete',
-                url: "/users/#{Request.extract_id_from_params(params)}"
-            },
-            user_assistants: {
-                request: 'get',
-                url: "/users/#{Request.extract_id_from_params(params)}/assistants"
-            },
-            user_assistants_create: {
-                request: 'post',
-                url: "/users/#{Request.extract_id_from_params(params)}/assistants"
-            },
-            user_assistants_delete_all: {
-                request: 'delete',
-                url: "/users/#{Request.extract_id_from_params(params)}/assistants"
-            },
-            user_schedulers: {
-                request: 'get',
-                url: "/users/#{Request.extract_id_from_params(params)}/schedulers"
-            },
-            user_schedulers_delete_all: {
-                request: 'delete',
-                url: "/users/#{Request.extract_id_from_params(params)}/schedulers"
-            },
+        def user_method_without_id_options
+          {users: 'fire_get', user_create: 'fire_post'}
+        end
+        def user_method_with_id_options params
+          {
+              user: {
+                  request: 'get',
+                  url: "/users/#{Request.extract_id_from_params(params)}"
+              },
+              user_update: {
+                  request: 'patch',
+                  url: "/users/#{Request.extract_id_from_params(params)}"
+              },
+              user_delete: {
+                  request: 'delete',
+                  url: "/users/#{Request.extract_id_from_params(params)}"
+              },
+              user_assistants: {
+                  request: 'get',
+                  url: "/users/#{Request.extract_id_from_params(params)}/assistants"
+              },
+              user_assistants_create: {
+                  request: 'post',
+                  url: "/users/#{Request.extract_id_from_params(params)}/assistants"
+              },
+              user_assistants_delete_all: {
+                  request: 'delete',
+                  url: "/users/#{Request.extract_id_from_params(params)}/assistants"
+              },
+              user_schedulers: {
+                  request: 'get',
+                  url: "/users/#{Request.extract_id_from_params(params)}/schedulers"
+              },
+              user_schedulers_delete_all: {
+                  request: 'delete',
+                  url: "/users/#{Request.extract_id_from_params(params)}/schedulers"
+              },
 
-        }
-      end
-      def method_with_multiple_id_options params
-        {
-            user_assistant_delete: {
-                url: "/users/#{params[:id]}/assistants/#{params[:assistant_id]}",
-                condition: (params[:id].blank? && params[:assistant_id].blank?)
-            },
-            user_schedulers_delete: {
-                url: "/users/#{params[:id]}/schedulers/#{params[:scheduler_id]}",
-                condition: (params[:id].blank? && params[:scheduler_id].blank?)
-            }
-        }
-      end
+          }
+        end
+        def user_method_with_multiple_id_options params
+          {
+              user_assistant_delete: {
+                  url: "/users/#{params[:id]}/assistants/#{params[:assistant_id]}",
+                  condition: (params[:id].blank? || params[:assistant_id].blank?)
+              },
+              user_schedulers_delete: {
+                  url: "/users/#{params[:id]}/schedulers/#{params[:scheduler_id]}",
+                  condition: (params[:id].blank? || params[:scheduler_id].blank?)
+              }
+          }
+        end
     end
   end
 end
